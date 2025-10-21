6b675496dbc5f2507908feee609ee0bbf1db26c4
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2025-10-21 02:41:24 -0400

    load data updated

--- load_data.py
+++ load_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import os
 
 DATASET_PATH = "dataset/celeba"
 
 def get_bbox_df():
-    bbox_df = pd.read_csv(DATASET_PATH+"/list_bbox_celeba.csv")
+    bbox_df = pd.read_csv(os.path.join(DATASET_PATH,"list_bbox_celeba.csv"))
     bbox_df.head()
     return bbox_df
 
