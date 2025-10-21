78b0c4cffbf84b69935be7c3dcd51b8e3624a84d
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2025-10-21 17:06:19 -0400

    model.py implemented

---
+++ model.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+from utils import prep_image
+
+
+def load_model_from_file(path):
+    if not os.path.exists(path):
+        raise FileExistsError(f"Model not found at {path}")
+    return load_model(path)
+
+
+def visualize_prediction(model, img_path):
+
+    if not os.path.exists(img_path):
+        print("!!! Image not Found !!!")
+        return
+    
+    img = cv2.imread(img_path)
+    
+    prepared_img, original_img_height, original_img_width = prep_image(img_path)
+
+    pred = model.predict(np.expand_dims(prepared_img / 255, axis=0))[0]
+
+    x, yb, bw, bh, conf = pred * [original_img_width, original_img_height, original_img_width, original_img_height, 1]
+
+    img_drawn = img.copy()
+    if conf > 0.5:
+        cv2.rectangle(img_drawn, (int(x), int(yb)), (int(x+bw), int(yb+bh)), (0,255,0), 2)
+        cv2.putText(img_drawn, f"conf={conf:.2f}", (10,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+    else:
+        cv2.putText(img_drawn, "no face", (10,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+
+    plt.imshow(img_drawn)
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == "__main__":
+    print("loading...")
+    model = load_model_from_file("model/face_detector_model.keras")
+    visualize_prediction(model, r"dataset\celeba\img_align_celeba\img_align_celeba\000001.jpg")
