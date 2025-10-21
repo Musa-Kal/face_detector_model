3bdf5c94c529e2aee02555d61abaf4323d5ff031
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2025-10-21 03:06:36 -0400

    data process done hopefully

--- model.py
+++ model.py
@@ -27,11 +27,23 @@ for i in range(NUM_SAMPLES):
         continue
 
     img = cv2.imread(img_path)
-    print(img)
-    print("===================")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    print(img)
-    h, w, _ = img.shape
-    print(h, w)
+    img_height, img_width, _ = img.shape
 
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
 
+    # normalizing bbox
+    x = image_bbox["x_1"] / img_width
+    y = image_bbox["y_1"] / img_height
+    width = image_bbox["width"] / img_width
+    height = image_bbox["height"] / img_height
+    conf = 1.0
+
+    # adding data
+    images.append(img / 255)
+    labels.append((x, y, width, height, conf))
+
+X = np.array(images, dtype=np.float32)
+y_ = np.array(labels, dtype=np.float32)
+
+print("=== DATA PROCESSED ===")
