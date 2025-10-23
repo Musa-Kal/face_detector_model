4d81be6af8c9678c68f1883f9d6c69b28a85e603
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-10-22 23:37:55 -0400

    updated

--- model_setup.py
+++ model_setup.py
@@ -1,10 +1,10 @@
 import os
 
 IMG_SIZE = 128
-NUM_SAMPLES = 10000
+NUM_SAMPLES = 20000
 SAMPLE_OFFSET = 10000
 
 MODEL_FOLDER = "model"
-TRAINED_MODEL_PATH = None#os.path.join(MODEL_FOLDER, "face_detector_model.keras")
+TRAINED_MODEL_PATH = os.path.join(MODEL_FOLDER, "face_detector_model.keras")
 DATASET_PATH = os.path.join("dataset", "celeba")
 IMAGE_PATH = os.path.join(DATASET_PATH, "img_align_celeba", "img_align_celeba")
