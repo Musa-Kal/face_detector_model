f1b4f61e32ea8b5610446ff98f7c895cc7b7e500
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-10-24 20:19:20 -0400

    CIFAR daaset loading function added to utils.py and dataset paths updated in model_setup.py.

--- model_setup.py
+++ model_setup.py
@@ -1,10 +1,11 @@
 import os
 
 IMG_SIZE = 128
-NUM_SAMPLES = 20000
-SAMPLE_OFFSET = 10000
+NUM_SAMPLES = 10
+SAMPLE_OFFSET = 0
 
 MODEL_FOLDER = "model"
 TRAINED_MODEL_PATH = os.path.join(MODEL_FOLDER, "face_detector_model.keras")
-DATASET_PATH = os.path.join("dataset", "celeba")
-IMAGE_PATH = os.path.join(DATASET_PATH, "img_align_celeba", "img_align_celeba")
+CELEBA_DATASET_PATH = os.path.join("dataset", "celeba")
+CIFAR_DATASET_PATH = os.path.join("dataset", "cifar10_data")
+IMAGE_PATH = os.path.join(CELEBA_DATASET_PATH, "img_align_celeba", "img_align_celeba")
--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import pickle
 import os
 import cv2
-from model_setup import IMG_SIZE, DATASET_PATH
+from model_setup import IMG_SIZE, CELEBA_DATASET_PATH, CIFAR_DATASET_PATH
 
 
 
 def get_bbox_df():
-    bbox_df = pd.read_csv(os.path.join(DATASET_PATH,"list_bbox_celeba.csv"))
+    bbox_df = pd.read_csv(os.path.join(CELEBA_DATASET_PATH,"list_bbox_celeba.csv"))
     bbox_df.head()
     return bbox_df
 
@@ -17,6 +18,19 @@ def prep_image(img_path, IMG_SIZE=IMG_SIZE):
 
     return cv2.resize(img, (IMG_SIZE, IMG_SIZE)), img_height, img_width
 
+def load_cifar10_batch(batch_name='data_batch_1'):
+    with open(os.path.join(CIFAR_DATASET_PATH, batch_name), 'rb') as f:
+        batch = pickle.load(f, encoding='latin1')
+    data = batch['data']        # shape: (10000, 3072)
+    labels = batch['labels']    # list of 10000 labels
+    # Reshape: each image is 32x32x3
+    images = data.reshape(-1, 3, 32, 32)
+    images = images.transpose(0, 2, 3, 1)  # shape → (N, 32, 32, 3)
+    return images, labels
+
 
 if __name__ == "__main__":
-    print(get_bbox_df())
+    # print(get_bbox_df())
+    img = load_cifar10_batch()[0][0]  # first image
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)           # convert color channels
+    img_resized = cv2.resize(img_bgr, (320, 320), interpolation=cv2.INTER_NEAREST)
