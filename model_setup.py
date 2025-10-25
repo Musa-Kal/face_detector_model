d8d59ad93a4ceb47ea87b7a60e39d2d882274af0
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-10-24 22:50:17 -0400

    best model so far turns out data was list_bbox.csv is inaccurate

--- model_setup.py
+++ model_setup.py
@@ -1,12 +1,12 @@
 import os
 
 IMG_SIZE = 128
-NUM_SAMPLES = 10
+NUM_SAMPLES = 20000
 SAMPLE_OFFSET = 0
-FACE_IMAGE_SPLIT_RATIO = 1
+FACE_IMAGE_SPLIT_RATIO = .45
 
 MODEL_FOLDER = "model"
-TRAINED_MODEL_PATH = os.path.join(MODEL_FOLDER, "face_detector_model.keras")
+TRAINED_MODEL_PATH = None#os.path.join(MODEL_FOLDER, "face_detector_model.keras")
 CELEBA_DATASET_PATH = os.path.join("dataset", "celeba")
 CIFAR_DATASET_PATH = os.path.join("dataset", "cifar10_data")
 IMAGE_PATH = os.path.join(CELEBA_DATASET_PATH, "img_align_celeba", "img_align_celeba")
